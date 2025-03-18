0d17ba506cd36cb1cc7e42c4a7d35954340f5ccb
AnanthuTD <ananthutd2021@gmail.com>
2025-03-18 13:56:04 +0530

    feat: added route to generate promotional video record and signed url to client upload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import eventsController, {
 	handleTestEvents,
 } from "../controllers/eventController";
 import protectedRoutes from "./protected";
+import interserviceRoutes from "./interService";
 import compression from "compression";
 import { getPreviewVideoDetails } from "../controllers/getVideoDetails";
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.use(compression());
 
+router.use('/interservice', interserviceRoutes)
 router.get("/test/events", handleTestEvents);
 router.get("/:workspaceId/events", eventsController);
 
--- src/routes/interService.ts
+++ src/routes/interService.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import  {
-  handleTestEvents,
-} from "../controllers/eventController";
+import { handleTestEvents } from "../controllers/eventController";
 import compression from "compression";
+import { createPromotionalVideo } from "../controllers/createPromotionalVideo.controller";
 
 const router = express.Router();
 
 router.use(compression());
 
 router.get("/get", handleTestEvents);
+router.post("/promotional-video", createPromotionalVideo);
 
 export default router;
