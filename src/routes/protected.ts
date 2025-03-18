bfa59ba2e0de5dfa6ac68b8110f87ac9b1551075
AnanthuTD <ananthutd2021@gmail.com>
2025-03-18 15:24:27 +0530

    feat: added route to get promotional videos

--- src/routes/protected.ts
+++ src/routes/protected.ts
@@ -14,27 +14,32 @@ import {
 	handleClearChatHistory,
 } from "../controllers/aiAgent.controller";
 import { VideoEditController } from "../controllers/videoEdit.controller";
-import { videoMoveController, videoShareController } from "../controllers/videoShareController";
+import {
+	videoMoveController,
+	videoShareController,
+} from "../controllers/videoShareController";
 import { deleteVideoController } from "../controllers/deleteVideo.controller";
 import { getLiveStreamToken } from "../controllers/liveStream.controller";
+import { getPromotionalVideos } from "../controllers/getPromotionalVideo.controller";
 
 const router = express.Router();
 
 router.use(passport.authenticate("jwt", { session: false }));
 
-router.get('/stream-key', getLiveStreamToken)
+router.get("/stream-key", getLiveStreamToken);
 router.get("/upload-presigned-url", VideoEditController.generatePresignedUrl);
-router.post('/:videoId/edit-success', VideoEditController.onSuccess)
+router.post("/:videoId/edit-success", VideoEditController.onSuccess);
 
 router.get("/search", searchVideosController);
 router.get("/search/autocomplete", autocompleteSearchVideosController);
+router.get("/promotionalVideos", getPromotionalVideos);
 
 router.post("/watch-later", WatchLaterController.add);
 router.delete("/:videoId/watch-later", WatchLaterController.remove);
 router.get("/watch-later", WatchLaterController.get);
 
 router.get("/:workspaceId", getVideos);
-router.delete("/:videoId", deleteVideoController)
+router.delete("/:videoId", deleteVideoController);
 
 router.get("/:videoId/video", getVideoDetails);
 
